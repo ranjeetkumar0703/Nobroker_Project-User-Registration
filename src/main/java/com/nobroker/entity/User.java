b7d0d20897d0a933a9ce870aa42b611a2ec86648
Ranjeet Kumar <rkumar054@outlook.com>
2024-01-22 16:48:05 +0530

    commit UserRegistration email verification1

--- src/main/java/com/nobroker/entity/User.java
+++ src/main/java/com/nobroker/entity/User.java
@@ -5,33 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class User{
 
-=======
-@Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Table(name = "users")
-public class User {
->>>>>>> e313d527fcd011cb656e0c7b01c968bce09ddcb4
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private long id;
     private String name;
     private String email;
-<<<<<<< HEAD
+
     private String password;
     private String mobile;
     private boolean emailVerified;
-=======
-    private String mobile;
-    private String password;
 
->>>>>>> e313d527fcd011cb656e0c7b01c968bce09ddcb4
 }
+
+
+
